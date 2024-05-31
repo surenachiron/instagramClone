9b884ff6d0457a6e38e025c5b24e505a39f4dcda
Mohammad Karimi <mohammadkarimi.swctw@gmail.com>
2024-05-31 16:19:50 +0330

    initial

---
+++ commitlint.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-empty': [2, 'never'],
+    'type-enum': [
+      2,
+      'always',
+      ['feat', 'fix', 'docs', 'chore', 'style', 'refactor', 'ci', 'test', 'revert', 'perf', 'vercel'],
+    ],
+  },
+};

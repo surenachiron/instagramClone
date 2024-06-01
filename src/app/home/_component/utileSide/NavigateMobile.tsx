3515ef41a963fde6d2abf828f254013a4977201e
Mohammad Karimi <mohammadkarimi.swctw@gmail.com>
2024-06-01 17:49:10 +0330

    fix: change width of navbar in mobile and increase height of navbar icons

--- src/app/home/_component/utileSide/NavigateMobile.tsx
+++ src/app/home/_component/utileSide/NavigateMobile.tsx
@@ -8,12 +8,12 @@ const NavigateMobile = () => {
   const pathname = usePathname();
 
   return (
-    <div className="desktop:hidden flex fixed z-30 inset-x-0 bottom-2 text-center justify-center">
-      <div className="backdrop-blur-3xl bg-[#ffffffc2] flex gap-x-8 p-2 rounded-xl">
+    <div className="desktop:hidden flex fixed z-30 inset-x-0 bottom-2 justify-center">
+      <div className="backdrop-blur-3xl bg-[#ffffffc2] flex justify-center gap-x-8 p-2 rounded-xl w-[65%]">
         {PagesDetail.map((page) => (
           <Link href={page.link} key={page.link}>
             <div
-              className={`flex items-start justify-start gap-x-2 ${pathname === page.link ? 'text-black' : 'text-grayMiddle'}`}
+              className={`flex items-start justify-start gap-x-2 h ${pathname === page.link ? 'text-black' : 'text-grayMiddle'}`}
             >
               {page.icon}
             </div>

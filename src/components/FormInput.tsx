c9f531e983baf5c0fa5d9dbb4b0b15d1a473ac39
Mohammad Karimi <mohammadkarimi.swctw@gmail.com>
2024-06-24 17:41:33 +0330

    feat: add reusable input with handling register of react-hook-form and showing error

---
+++ src/components/FormInput.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import { FormInputProps } from '@/types/formTypes';
+import Input from './Input';
+
+const FormInput: React.FC<FormInputProps> = ({
+  type,
+  placeholder,
+  name,
+  register,
+  error,
+  valueAsNumber,
+  classes = 'w-full py-3 px-2',
+  parentClasses,
+  ...props
+}) => {
+  return (
+    <div className={`w-full h-full flex flex-col gap-1 ${parentClasses}`}>
+      <Input
+        type={type!}
+        name={name}
+        placeholder={placeholder}
+        register={register}
+        valueAsNumber={valueAsNumber}
+        classes={classes}
+        {...props}
+      />
+      {error && <span className="text-red-600">{error.message}</span>}
+    </div>
+  );
+};
+
+export default FormInput;

08695cac20bf97055eb5469302846bcd5e94b350
Mohammad Karimi <mohammadkarimi.swctw@gmail.com>
2024-06-24 17:39:19 +0330

    feat: add feature password visibility and showing loading

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,14 +1,42 @@
 'use client';
 
+import { useEffect, useState } from 'react';
+import { RotateSpinner, SpinnerT } from './Icons/RotateSpinner';
+
 interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
+  loading?: boolean;
+  direction?: 'row' | 'empty';
   classes?: string;
+  Spinner?: SpinnerT;
 }
 
-const Button: React.FC<Props> = ({ classes, children, onClick, ...props }) => {
+const Button: React.FC<Props> = ({
+  classes,
+  loading = false,
+  direction = 'empty',
+  children,
+  onClick,
+  Spinner,
+  ...props
+}) => {
+  const [output, setOutput] = useState<React.ReactElement>(<>{children}</>);
+
+  useEffect(() => {
+    if (loading) {
+      if (direction === 'row') {
+        setOutput(
+          <div className="flex items-center gap-x-1">
+            {children} <RotateSpinner {...Spinner} />
+          </div>
+        );
+      } else setOutput(<RotateSpinner />);
+    } else setOutput(<>{children}</>);
+  }, [loading, direction]);
+
   return (
     <button {...props} className={`flex justify-center items-center font-regular ${classes}`} onClick={onClick}>
-      {children}
+      {output}
     </button>
   );
 };

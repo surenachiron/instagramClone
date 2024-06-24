5d237fc63f747cd9d281ba98ee3f14e393c0e56b
Mohammad Karimi <mohammadkarimi.swctw@gmail.com>
2024-06-24 18:05:50 +0330

    perf: change name one of props

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -1,18 +1,18 @@
 'use client';
 
 import { IoIosSearch } from 'react-icons/io';
-import { useUtilAction, useUtilStore } from '@store/util';
+import { useUtilAction, useUtilStore } from '@/store/util';
 
-type Props = { bg: 'Back' | 'Middle' | 'Light'; padH: number };
+type Props = { bg: 'Back' | 'Middle' | 'Light'; paddingH: number };
 
-const SearchBox = ({ bg, padH }: Props) => {
+const SearchBox = ({ bg, paddingH }: Props) => {
   const { changeTextSearch } = useUtilAction();
   const { textSearch } = useUtilStore();
 
   return (
     <div
       className={`flex gap-1 w-full h-full rounded-2xl items-center bg-gray${bg} text-grayMiddle px-2`}
-      style={{ padding: `${padH}px 0` }}
+      style={{ padding: `${paddingH}px 0` }}
     >
       <IoIosSearch className="cursor-pointer text-2xl text-grayMiddle" />
       <input

af7df4dd451288dc2a68b227de533a2c22a52c78
Mohammad Karimi <mohammadkarimi.swctw@gmail.com>
2024-06-24 17:24:31 +0330

    fix: wrong in path of hostname

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     return [{ source: '/', destination: '/home', permanent: true }];
   },
   images: {
-    remotePatterns: [{ hostname: 'via.placeholder.com' }, { hostname: 'rawإrrhqbuqlxiqvuhbmc.supabase.co' }],
+    remotePatterns: [{ hostname: 'via.placeholder.com' }, { hostname: 'rawrrhqbuqlxiqvuhbmc.supabase.co' }],
   },
   reactStrictMode: true,
 };

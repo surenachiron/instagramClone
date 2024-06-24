92b968525e5628986f8300b15386f4a55d7c9c9a
Mohammad Karimi <mohammadkarimi.swctw@gmail.com>
2024-06-24 18:13:52 +0330

    feat: add loading state and action

--- src/store/util.ts
+++ src/store/util.ts
@@ -1,16 +1,22 @@
 import { create } from 'zustand';
 
+type loading = { [key: string]: boolean };
+
 type TUtilStore = {
   textSearch: string;
+  loading: loading;
   actions: {
     changeTextSearch: (text: string) => void;
+    setLoading: (identifier: string, val: boolean) => void;
   };
 };
 
 export const useUtilStore = create<TUtilStore>()((set) => ({
   textSearch: '',
+  loading: { submit: false },
   actions: {
     changeTextSearch: (text) => set(() => ({ textSearch: text })),
+    setLoading: (identifier, val) => set((state) => ({ loading: { ...state.loading, [identifier]: val } })),
   },
 }));
 

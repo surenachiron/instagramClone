135c0a77fa317c9204ff91174877fd177ba45a4f
Mohammad Karimi <mohammadkarimi.swctw@gmail.com>
2024-06-27 19:36:13 +0330

    feat: formatted

--- src/components/GradientContainer.tsx
+++ src/components/GradientContainer.tsx
@@ -10,11 +10,7 @@ type Props = {
 const GradientContainer = ({ children, classes, childClasses, borderGradient = false }: Props) => {
   return (
     <div className={`bg-gradient-to-r from-[#fbaa47] via-[#D91A46] to-[#A60F93] p-0.5 rounded-full ${classes}`}>
-      {borderGradient ? (
-        <div className={`bg-white p-0.5 rounded-full ${childClasses}`}>{children}</div>
-      ) : (
-        <>{children}</>
-      )}
+      {borderGradient ? <div className={`bg-white p-0.5 rounded-full ${childClasses}`}>{children}</div> : children}
     </div>
   );
 };

623b0fa480f36159bba3f2636d3a6fa47a5f1997
Mohammad Karimi <mohammadkarimi.swctw@gmail.com>
2024-06-27 19:14:55 +0330

    feat: set correctly path for profile to go the now user

--- src/app/home/_component/utileSide/NavigateMobile.tsx
+++ src/app/home/_component/utileSide/NavigateMobile.tsx
@@ -3,22 +3,25 @@
 import Link from 'next/link';
 import { PagesDetail } from './PagesList';
 import { usePathname } from 'next/navigation';
+import { useUtilStore } from '@/store/util';
 
 const NavigateMobile = () => {
+  const { username } = useUtilStore();
   const pathname = usePathname();
 
   return (
     <div className="desktop:hidden flex justify-center fixed z-30 inset-x-5 bottom-1">
       <div className="bg-white flex justify-around gap-x-8 p-2 rounded-xl w-full tablet:w-[65%] shadow-lg">
-        {PagesDetail.map((page) => (
-          <Link href={page.link} key={page.link}>
-            <div
-              className={`flex items-start justify-start gap-x-2 h ${pathname === page.link ? 'text-black' : 'text-grayMiddle'}`}
-            >
-              {page.icon}
-            </div>
-          </Link>
-        ))}
+        {username.length > 1 &&
+          PagesDetail.map((page) => (
+            <Link href={page.link === '/profile/' ? `/profile/${username}` : page.link} key={page.link}>
+              <div
+                className={`flex items-start justify-start gap-x-2 h ${pathname === page.link + username ? 'text-black' : 'text-grayMiddle'}`}
+              >
+                {page.icon}
+              </div>
+            </Link>
+          ))}
       </div>
     </div>
   );
--- src/app/home/_component/utileSide/PagesList.tsx
+++ src/app/home/_component/utileSide/PagesList.tsx
@@ -2,6 +2,7 @@
 
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
+import { useUtilStore } from '@/store/util';
 import { GoHomeFill } from 'react-icons/go';
 import { IoPersonCircle } from 'react-icons/io5';
 import { MdOutlineExplore } from 'react-icons/md';
@@ -11,24 +12,26 @@ export const PagesDetail: { name: string; link: string; icon: React.ReactNode }[
   { name: 'Home', link: '/home', icon: <GoHomeFill className="text-3xl tablet:text-2xl" /> },
   { name: 'Explore', link: '/explore', icon: <MdOutlineExplore className="text-3xl tablet:text-2xl" /> },
   { name: 'Reels', link: '/reels', icon: <RiMovie2Line className="text-3xl tablet:text-2xl" /> },
-  { name: 'Profile', link: '/profile', icon: <IoPersonCircle className="text-3xl tablet:text-2xl" /> },
+  { name: 'Profile', link: '/profile/', icon: <IoPersonCircle className="text-3xl tablet:text-2xl" /> },
 ];
 
 const PagesList = () => {
+  const { username } = useUtilStore();
   const pathname = usePathname();
 
   return (
     <div className="flex flex-col items-start gap-3 w-full">
-      {PagesDetail.map((page) => (
-        <Link href={page.link} key={page.link}>
-          <div
-            className={`flex items-start justify-start gap-x-2 ${pathname === page.link ? 'text-black' : 'text-grayMiddle'}`}
-          >
-            {page.icon}
-            <h3 className="font-regular">{page.name}</h3>
-          </div>
-        </Link>
-      ))}
+      {username.length > 1 &&
+        PagesDetail.map((page) => (
+          <Link href={page.link === '/profile/' ? `/profile/${username}` : page.link} key={page.link}>
+            <div
+              className={`flex items-start justify-start gap-x-2 ${pathname === page.link ? 'text-black' : 'text-grayMiddle'}`}
+            >
+              {page.icon}
+              <h3 className="font-regular">{page.name}</h3>
+            </div>
+          </Link>
+        ))}
     </div>
   );
 };

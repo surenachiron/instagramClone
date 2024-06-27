021ac0aff1f5278fa16879126aa88eb73a0dd9b0
Mohammad Karimi <mohammadkarimi.swctw@gmail.com>
2024-06-27 19:27:27 +0330

    feat: add username state

--- src/store/util.ts
+++ src/store/util.ts
@@ -5,18 +5,22 @@ type loading = { [key: string]: boolean };
 type TUtilStore = {
   textSearch: string;
   loading: loading;
+  username: string;
   actions: {
     changeTextSearch: (text: string) => void;
     setLoading: (identifier: string, val: boolean) => void;
+    setUsername: (val: string) => void;
   };
 };
 
 export const useUtilStore = create<TUtilStore>()((set) => ({
   textSearch: '',
   loading: { submit: false },
+  username: '',
   actions: {
     changeTextSearch: (text) => set(() => ({ textSearch: text })),
     setLoading: (identifier, val) => set((state) => ({ loading: { ...state.loading, [identifier]: val } })),
+    setUsername: (val) => set(() => ({ username: val })),
   },
 }));
 

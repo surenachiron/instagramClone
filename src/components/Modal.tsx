d41e395b78d67b4d470b6d19c7b7312e0aad98b8
Mohammad Karimi <mohammadkarimi.swctw@gmail.com>
2024-06-27 19:38:06 +0330

    feat: more reusable

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -5,25 +5,27 @@ import Button from './Button';
 
 type Props = {
   onOpen: () => void;
-  icon: React.ReactElement;
+  onClose?: () => void;
+  icon?: React.ReactElement | string;
   iconStyle?: string;
+  parentIconStyle?: string;
   title: string;
   children: React.ReactNode;
 };
 
 const Modal = forwardRef<HTMLDialogElement, Props>(
-  ({ onOpen, icon, iconStyle = 'bg-blue p-[.3rem]', title, children }, ref) => {
+  ({ onOpen, onClose, icon, iconStyle = 'rounded-full bg-blue p-[.3rem]', parentIconStyle, title, children }, ref) => {
     // for forwardRef
     Modal.displayName = title;
 
     return (
       <div className="z-50">
-        <Button classes="pt-2 pl-3 absolute right-0 bottom-0" onClick={onOpen}>
-          <div className={`rounded-full ${iconStyle}`}>{icon}</div>
+        <Button classes={`${parentIconStyle}`} onClick={onOpen}>
+          <div className={`${iconStyle}`}>{icon}</div>
         </Button>
         <dialog id="my_modal_3" className="modal" ref={ref}>
           <div className="modal-box bg-white cursor-auto">
-            <form method="dialog">
+            <form method="dialog" onClick={onClose}>
               <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
             </form>
             <h3 className="font-bold text-lg text-center w-full">{title}</h3>

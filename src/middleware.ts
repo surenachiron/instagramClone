24e58b655c59d9c6a9ca78a47e22238a31242844
Mohammad Karimi <mohammadkarimi.swctw@gmail.com>
2024-07-04 20:02:03 +0330

    feat: add posts paths to matcher

--- src/middleware.ts
+++ src/middleware.ts
@@ -45,5 +45,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/auth/:path*', '/', '/profile', '/posts/*'],
+  matcher: ['/auth/:path*', '/', '/profile', '/posts/:path*'],
 };

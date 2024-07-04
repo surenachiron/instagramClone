00cf54ec6a30c8c4daae5034120e98c1f8aeba54
Mohammad Karimi <mohammadkarimi.swctw@gmail.com>
2024-07-04 19:57:21 +0330

    feat: reusability for showing max prop

--- src/components/TextArea.tsx
+++ src/components/TextArea.tsx
@@ -5,7 +5,7 @@ import { UseFormRegister } from 'react-hook-form';
 
 interface Props extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {
   name: string;
-  max: string;
+  max?: string;
   valueDefault?: string;
   register?: UseFormRegister<any>;
   error?: string | undefined;
@@ -22,13 +22,15 @@ const TextArea = ({ register, error, name, valueDefault, max, labelContent, labe
       <div className="mt-2 relative">
         <textarea
           spellCheck={false}
-          className={`w-full pl-2 pr-14 py-1 bg-white border font-light border-grayBack rounded-xl resize-none text-black ${bioLength! > +max && 'border-red-600 outline-none'}`}
+          className={`w-full pl-2 pr-14 py-1 bg-white border font-light border-grayBack rounded-xl resize-none text-black ${max && bioLength! > +max && 'border-red-600 outline-none'}`}
           {...(register && { ...register(name!, { onChange: (e) => setBioLength(e.target.value.length) }) })}
           {...props}
         />
-        <span className={`absolute bottom-2 right-5 ${bioLength! > +max && 'text-red-600'}`}>
-          {bioLength ? bioLength : 0} / {max}
-        </span>
+        {max && (
+          <span className={`absolute bottom-2 right-5 ${bioLength! > +max && 'text-red-600'}`}>
+            {bioLength ? bioLength : 0} / {max}
+          </span>
+        )}
       </div>
       {error && <span className="text-red-600">{error}</span>}
     </div>

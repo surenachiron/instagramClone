a67b333e28837440f3d085c8dc293d57957e71e7
Mohammad Karimi <mohammadkarimi.swctw@gmail.com>
2024-07-04 19:51:36 +0330

    feat: add "link" props for more reusability to what page back should go

--- src/components/BackPage.tsx
+++ src/components/BackPage.tsx
@@ -6,13 +6,17 @@ import { FaArrowLeft } from 'react-icons/fa6';
 
 type Props = {
   className?: string;
+  link?: string;
 };
 
-const BackPage = ({ className }: Props) => {
+const BackPage = ({ className, link }: Props) => {
   const router = useRouter();
 
   return (
-    <Button classes={`p-1 rounded-lg w-fit justify-start items-start ${className}`} onClick={() => router.back()}>
+    <Button
+      classes={`p-1 rounded-lg w-fit justify-start items-start ${className}`}
+      onClick={() => (link ? router.push(link) : router.back())}
+    >
       <FaArrowLeft className="text-xl" />
     </Button>
   );

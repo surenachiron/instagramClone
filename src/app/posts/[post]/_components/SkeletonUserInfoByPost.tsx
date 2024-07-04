b7401586f465a7f0d6e29f492281690e3fe216b3
Mohammad Karimi <mohammadkarimi.swctw@gmail.com>
2024-07-04 22:44:28 +0330

    feat: loading skeleton for show a little view of user profile

---
+++ src/app/posts/[post]/_components/SkeletonUserInfoByPost.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import Box from '@/components/Box';
+import Skeleton from '@/components/Skeleton';
+
+const SkeletonUserInfoByPost = () => {
+  return (
+    <Box classes={`absolute top-[34px] p-[6px] w-[350px] shadow-xl`} align="">
+      <div className="flex items-center justify-start gap-2 mx-3 group relative">
+        <Skeleton parentClass="w-[55px] h-[55px]" classes="w-full h-full" radius="full" />
+        <div className="flex flex-col gap-y-2 justify-center w-fit">
+          <Skeleton classes="py-0.5 w-[60px]" radius="full" />
+          <Skeleton classes="py-0.5 w-[90px]" radius="full" />
+        </div>
+      </div>
+      <hr className="my-2" />
+      <div className="grid grid-cols-12 gap-1">
+        {Array.from({ length: 3 }).map((post, index) => (
+          <Skeleton parentClass="col-span-4 h-36" classes="w-full h-full" key={index} />
+        ))}
+      </div>
+    </Box>
+  );
+};
+
+export default SkeletonUserInfoByPost;

dcb36cadc512a5a25ddfc1a728b0f838228df302
Mohammad Karimi <mohammadkarimi.swctw@gmail.com>
2024-07-04 19:47:38 +0330

    feat: change file name

--- src/hooks/FilePath.tsx
+++ src/hooks/FilePath.tsx
@@ -1,9 +1,9 @@
 'use client';
 
-const useFilePath = (url: string, baseOf: string): string => {
+const FilePath = (url: string, baseOf: string): string => {
   const startIndex = url.indexOf(baseOf);
   const fileName = url.substring(startIndex + baseOf.length);
   return fileName;
 };
 
-export default useFilePath;
+export default FilePath;

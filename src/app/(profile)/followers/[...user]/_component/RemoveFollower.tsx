52cf1a9844a70f87642cd42caaa7fd54a1d71ee2
Mohammad Karimi <mohammadkarimi.swctw@gmail.com>
2024-07-06 19:06:47 +0330

    feat: add remove follower

---
+++ src/app/(profile)/followers/[...user]/_component/RemoveFollower.tsx
@@ -0,0 +1,45 @@
+'use client';
+
+import { useState } from 'react';
+import Button from '@/components/Button';
+import { supabaseClient } from '@/supabase/utils/client';
+
+type Props = {
+  followerID: string;
+  followedID: string;
+  privateArea?: boolean;
+};
+
+const RemoveFollower = ({ followerID, followedID, privateArea }: Props) => {
+  const [loading, setLoading] = useState(false);
+  const [result, setResult] = useState(false);
+  const supabase = supabaseClient();
+
+  async function unFollowing() {
+    setLoading(true);
+    const { error } = await supabase
+      .from('follows')
+      .delete()
+      .eq('follower_id', followerID)
+      .eq('followed_id', followedID);
+    if (!error) setResult(true);
+    setLoading(false);
+  }
+
+  return (
+    <>
+      {privateArea && (
+        <Button
+          classes="bg-grayBack text-black py-1 px-3 rounded-lg text-sm"
+          loading={loading}
+          onClick={unFollowing}
+          disabled={result}
+        >
+          Remove
+        </Button>
+      )}
+    </>
+  );
+};
+
+export default RemoveFollower;

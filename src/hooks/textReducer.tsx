ad3abe96b87410dd8a2b45f560144e6e14db323c
Mohammad Karimi <mohammadkarimi.swctw@gmail.com>
2024-07-14 19:37:49 +0330

    fix: add condition to handle text length and append additionally string if necessary

--- src/hooks/textReducer.tsx
+++ src/hooks/textReducer.tsx
@@ -1,5 +1,5 @@
 type Props = { text: string; min?: number; max?: number; additionally?: string };
 
 export default function textReducer({ text, min = 0, max = text.length, additionally = '' }: Props) {
-  return text.slice(min, max) + additionally;
+  if (text && text.length > 0) return text.slice(min, max) + (text.length > max ? additionally : '');
 }

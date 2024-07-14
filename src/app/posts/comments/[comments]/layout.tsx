60a11a719e55ed82455b8dafd32970942ea0d76f
Mohammad Karimi <mohammadkarimi.swctw@gmail.com>
2024-07-14 22:59:15 +0330

    chore: delete params from params

--- src/app/posts/comments/[comments]/layout.tsx
+++ src/app/posts/comments/[comments]/layout.tsx
@@ -18,14 +18,14 @@ export async function generateMetadata({ params }: { params: { comments: string 
   };
 }
 
-export default function PostsLayout({ children, params }: { children: React.ReactNode; params: { comments: string } }) {
+export default function PostsLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="grid gap-3 grid-cols-1 tablet:grid-cols-2 desktop:grid-cols-4 tablet:container py-0 tablet:py-5 bg-white">
       <UtileSide />
       <div className="col-span-1 tablet:col-span-3">
         <div className="text-start block tablet:hidden">
           <div className="flex justify-between items-center bg-white text-black py-2 px-3 sticky top-0 border-b border-l-grayMiddle">
-            <BackPage className="bg-white left-3" link={`/posts/${params.comments}`} />
+            <BackPage className="bg-white left-3" />
             <h3 className="font-bold">Comment</h3>
             <div className="w-[30px]"> </div>
           </div>

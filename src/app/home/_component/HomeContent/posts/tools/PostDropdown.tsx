e20721988198be4388c6badf63edde030676a222
Mohammad Karimi <mohammadkarimi.swctw@gmail.com>
2024-07-14 23:46:52 +0330

    feat: add dropdown for more tools in posts

---
+++ src/app/home/_component/HomeContent/posts/tools/PostDropdown.tsx
@@ -0,0 +1,42 @@
+'use client';
+
+import { IoIosMore } from 'react-icons/io';
+import CopyLink from '@/components/CopyLink';
+import Dropdown from '@/components/Dropdown';
+import FollowUser from '@/app/(profile)/profile/[username]/_component/info/FollowUser';
+import DeletePost from '../../../../../posts/[post]/_components/tools/DeletePost';
+
+type Props = {
+  postId: string;
+  privateUser: boolean;
+  userID: string;
+  profileID: string;
+};
+
+const PostDropdown = ({ postId, userID, profileID, privateUser = false }: Props) => {
+  return (
+    <>
+      {privateUser === false ? (
+        <Dropdown
+          dropText={<IoIosMore className="text-2xl text-white mix-blend-difference" />}
+          items={[
+            { element: <DeletePost postId={postId} /> },
+            { element: <CopyLink customPath={`/posts/${postId}`} /> },
+          ]}
+        />
+      ) : (
+        <Dropdown
+          dropText={<IoIosMore className="text-2xl text-white mix-blend-difference" />}
+          items={[
+            { element: <CopyLink customPath={`/posts/${postId}`} /> },
+            {
+              element: <FollowUser user_id={userID} user_profile={profileID} />,
+            },
+          ]}
+        />
+      )}
+    </>
+  );
+};
+
+export default PostDropdown;

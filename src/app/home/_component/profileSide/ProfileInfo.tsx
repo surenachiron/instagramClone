d4e72b99f8e4b3e84274aa04fcfe86389547a388
Mohammad Karimi <mohammadkarimi.swctw@gmail.com>
2024-07-14 23:36:33 +0330

    feat: showing user's info in home page

--- src/app/home/_component/profileSide/ProfileInfo.tsx
+++ src/app/home/_component/profileSide/ProfileInfo.tsx
@@ -1,90 +1,63 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import Box from '@/components/Box';
-import Button from '@/components/Button';
 import GradientContainer from '@/components/GradientContainer';
+import UtilProfileBut, { ButtonsProfileProps } from '@/app/(profile)/profile/[username]/_component/info/UtilProfileBut';
+import textReducer from '@/hooks/textReducer';
 
-type Props = { buttons: { primaryText: string; secondaryText?: string; bgColor?: string }[] };
+type profileHomeInfoProps = {
+  profile: { name: string; username: string; bio: string; avatar: string };
+  posts?: { id: string; media_url: string }[];
+  buttons: ButtonsProfileProps;
+};
 
-const ProfileInfo = ({ buttons }: Props) => {
+const ProfileInfo = ({ profile, buttons, posts }: profileHomeInfoProps) => {
+  const bio = textReducer({ text: profile.bio, min: 0, max: 60, additionally: '...' });
+
   return (
-    <Box classes="relative px-6 gap-2 py-8 rounded-md w-full">
+    <Box classes="relative px-6 gap-2 pt-8 pb-4 rounded-md w-full">
       <div className="absolute top-[-45px]">
         <GradientContainer classes="p-0.5" childClasses="p-0.5" borderGradient={true}>
-          <Image src={'/anonymous.png'} alt="test image" width={70} height={70} className="rounded-full" />
+          <Image
+            src={profile.avatar ? profile.avatar : '/anonymous.png'}
+            alt="test image"
+            width={70}
+            height={70}
+            className="rounded-full"
+          />
         </GradientContainer>
       </div>
-
       <div className="text-center my-2">
-        <h3 className="text-xl font-bold text-black">Mohammad Karimi</h3>
-        <p className="text-sm">maybe a programmer.</p>
-      </div>
-
-      <div className="flex flex-wrap justify-start gap-2">
-        {buttons.map((but) => (
-          <Button
-            key={but.primaryText}
-            classes={`flex-col bg-grayLight ${but?.bgColor} text-black px-3 py-1 rounded-lg w-fit`}
-          >
-            {but.secondaryText ? (
-              <div className="flex flex-col items-center justify-center">
-                <h4 className="text-md font-bold">{but.primaryText}</h4>
-                <p className="text-sm text-[#a1a1a1]">{but.secondaryText}</p>
-              </div>
-            ) : (
-              <p className="text-sm text-[#a1a1a1]">{but.primaryText}</p>
-            )}
-          </Button>
-        ))}
-      </div>
-
-      <div className="flex flex-col gap-2 my-4 w-full">
-        <p className="text-sm font-bold text-grayMiddle">posts - 29</p>
-        <div className="grid grid-cols-12 gap-2">
-          {buttons.map((butTest) => (
-            <div
-              key={butTest.primaryText}
-              className="col-span-3 mobile:col-span-3 tablet:col-span-2 desktop:col-span-4"
-            >
-              <Image src={'/anonymous.png'} alt="test" className="rounded-lg w-full" width={70} height={70} />
-            </div>
-          ))}
-          {buttons.map((butTest) => (
-            <div
-              key={butTest.primaryText}
-              className="col-span-3 mobile:col-span-3 tablet:col-span-2 desktop:col-span-4"
-            >
-              <Image src={'/anonymous.png'} alt="test" className="rounded-lg w-full" width={70} height={70} />
-            </div>
-          ))}
-          {buttons.map((butTest) => (
-            <div
-              key={butTest.primaryText}
-              className="col-span-3 mobile:col-span-3 tablet:col-span-2 desktop:col-span-4"
-            >
-              <Image src={'/anonymous.png'} alt="test" className="rounded-lg w-full" width={70} height={70} />
-            </div>
-          ))}
-          {buttons.map((butTest) => (
-            <div
-              key={butTest.primaryText}
-              className="col-span-3 mobile:col-span-3 tablet:col-span-2 desktop:col-span-4"
-            >
-              <Image src={'/anonymous.png'} alt="test" className="rounded-lg w-full" width={70} height={70} />
-            </div>
-          ))}
-          {buttons.map((butTest) => (
-            <div
-              key={butTest.primaryText}
-              className="col-span-3 mobile:col-span-3 tablet:col-span-2 desktop:col-span-4"
-            >
-              <Image src={'/anonymous.png'} alt="test" className="rounded-lg w-full" width={70} height={70} />
-            </div>
-          ))}
-        </div>
-        <div className="flex justify-center my-2">
-          <Button classes="text-sm rounded-lg bg-grayLight w-fit px-2 py-1">Hide your posts</Button>
+        <h3 className="text-xl font-bold text-black">{profile.username}</h3>
+        <div className="w-full text-start mt-1">
+          <p className="text-sm">{bio}</p>
         </div>
       </div>
+      <div className="grid grid-cols-2 justify-start gap-2 w-full">
+        <UtilProfileBut buttons={buttons} />
+      </div>
+      {posts && posts.length > 0 && (
+        <div className="flex flex-col gap-2 my-4 w-full">
+          <p className="text-sm font-bold text-grayMiddle">posts - {posts?.length}</p>
+          <div className="grid grid-cols-12 gap-2">
+            {posts.map((post) => (
+              <Link
+                href={`/posts/${post.id}`}
+                key={post.id}
+                className="col-span-3 mobile:col-span-3 tablet:col-span-2 desktop:col-span-4"
+              >
+                <Image
+                  src={post.media_url}
+                  alt={`image for post of ${post.id}`}
+                  className="rounded-lg w-[100px] h-[100px] bg-grayMiddle"
+                  width={70}
+                  height={70}
+                />
+              </Link>
+            ))}
+          </div>
+        </div>
+      )}
     </Box>
   );
 };
--- src/app/home/_component/profileSide/index.tsx
+++ src/app/home/_component/profileSide/index.tsx
@@ -1,10 +1,40 @@
 import Box from '@/components/Box';
 import ProfileInfo from './ProfileInfo';
+import { supabaseServer } from '@/supabase/utils/server';
+import { cookies } from 'next/headers';
 
-const ProfileSide = () => {
+export const revalidate = 240;
+
+const ProfileSide = async () => {
+  const supabase = supabaseServer();
+  const enterUsername = cookies().get('username')?.value as string;
+  const { data } = await supabase
+    .from('profiles')
+    .select(
+      `*,
+      posts(id, media_url)
+      `
+    )
+    .eq('user_name', enterUsername)
+    .limit(1, { foreignTable: 'posts' })
+    .single();
+
   return (
     <Box classes="col-span-1 h-fit rounded-md desktop:flex hidden">
-      <ProfileInfo buttons={[{ primaryText: 'Edit Profile' }, { primaryText: 'Share profile' }]} />
+      {data ? (
+        <ProfileInfo
+          profile={{
+            avatar: data?.avatar_url as string,
+            bio: data.bio as string,
+            name: data.full_name as string,
+            username: data.user_name as string,
+          }}
+          posts={data.posts}
+          buttons={[{ primaryText: 'Edit Profile', link: '/profile/edit' }, { primaryText: 'Share profile' }]}
+        />
+      ) : (
+        <div className="p-2">Something went wrong, Please check your internet connection.</div>
+      )}
     </Box>
   );
 };

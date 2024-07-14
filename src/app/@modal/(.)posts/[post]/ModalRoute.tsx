6258a849c05b22f4e4a1889dbe1b07169f9613ca
Mohammad Karimi <mohammadkarimi.swctw@gmail.com>
2024-07-14 23:27:10 +0330

    feat: add Modal for showing posts in intersection route

---
+++ src/app/@modal/(.)posts/[post]/ModalRoute.tsx
@@ -0,0 +1,47 @@
+'use client';
+
+import { type ElementRef, MouseEvent, RefObject, useEffect, useRef } from 'react';
+import { useRouter } from 'next/navigation';
+import { createPortal } from 'react-dom';
+import { CgClose } from 'react-icons/cg';
+import Button from '@/components/Button';
+
+export function ModalRoute({ children }: { children: React.ReactNode }) {
+  const router = useRouter();
+  const dialogRef = useRef<ElementRef<'dialog'>>(null);
+
+  useEffect(() => {
+    if (!dialogRef.current?.open) {
+      dialogRef.current?.showModal();
+    }
+  }, []);
+
+  function onDismiss() {
+    router.back();
+  }
+
+  const handleBackdropClick = (
+    event: MouseEvent<HTMLDivElement>,
+    modalRef: RefObject<HTMLDialogElement>,
+    closeModal: () => void
+  ) => {
+    if (modalRef.current && modalRef.current === event.target) {
+      closeModal();
+    }
+  };
+
+  return createPortal(
+    <div className="modal-backdrop" onClick={(e) => handleBackdropClick(e, dialogRef, onDismiss)}>
+      <dialog ref={dialogRef} className="modal text-white" onClose={onDismiss}>
+        <Button
+          onClick={onDismiss}
+          classes="hidden desktop:block absolute right-[35px] top-[10px] p-1 rounded-full border bg-white"
+        >
+          <CgClose className="text-xl text-black" />
+        </Button>
+        {children}
+      </dialog>
+    </div>,
+    document.getElementById('modal-root')!
+  );
+}

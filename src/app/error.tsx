e9cabd99ce6583fcda53caef2ad2f91bab12b121
Mohammad Karimi <mohammadkarimi.swctw@gmail.com>
2024-07-14 22:56:12 +0330

    chore: update error

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -3,7 +3,7 @@
 export default function Error({ reset }: { error: Error & { digest?: string }; reset: () => void }) {
   return (
     <div>
-      <h2>Something went wrong!</h2>
+      <h2>Something went wrong, Please check your internet connection.!</h2>
       <button onClick={() => reset()}>Try again</button>
     </div>
   );

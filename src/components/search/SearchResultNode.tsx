4ebc63a5bc3ab29f07c6fd34402ca8d3a9d28875
Mohammad Karimi <mohammadkarimi.swctw@gmail.com>
2024-07-14 19:48:46 +0330

    feat: show search result

---
+++ src/components/search/SearchResultNode.tsx
@@ -0,0 +1,46 @@
+import Link from 'next/link';
+import { resultType } from './SearchBox';
+import Image from 'next/image';
+import { MdPersonSearch } from 'react-icons/md';
+
+export const SearchResultNode = ({ result, classes }: { result: resultType; classes?: string }) => {
+  return (
+    <div className={`w-full h-[240px] border rounded-lg  bg-white transition-all fade-in ${classes}`}>
+      {result?.length !== 0 ? (
+        typeof result === 'string' ? (
+          <div className="w-full h-full flex flex-col items-center justify-center">
+            <p className="text-sm text-bold text-black">{result}</p>
+          </div>
+        ) : (
+          <div className="flex flex-col justify-start w-full h-full gap-y-2 overflow-auto customScroll ">
+            {result?.map((user) => (
+              <Link
+                href={`profile/${user.user_name}`}
+                key={user.user_id}
+                className="flex gap-x-2 items-center w-fit group"
+              >
+                <Image
+                  src={user.avatar_url ? user.avatar_url : '/anonymous.png'}
+                  alt={`avatar of ${user.user_name}`}
+                  width={100}
+                  height={100}
+                  className="w-[35px] h-[35px] rounded-full"
+                />
+                <div className="flex flex-col justify-center">
+                  <p className="text-base">{user.user_name}</p>
+                  <p className="text-sm">{user.full_name}</p>
+                </div>
+              </Link>
+            ))}
+          </div>
+        )
+      ) : (
+        <div className="w-full h-full flex flex-col items-center justify-center">
+          <MdPersonSearch className="text-lg text-black" />
+          <p className="text-xl text-bold text-black">No searches just yet</p>
+          <p className="text-sm">Search for your favorite user</p>
+        </div>
+      )}
+    </div>
+  );
+};

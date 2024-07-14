669bf88e4b522205f05a013e57ae8bcee2f6fb82
Mohammad Karimi <mohammadkarimi.swctw@gmail.com>
2024-07-14 23:34:03 +0330

    feat: add user's avatar instead of anonymous picture

--- src/app/home/_component/utileSide/NavigateMobile.tsx
+++ src/app/home/_component/utileSide/NavigateMobile.tsx
@@ -1,8 +1,10 @@
 'use client';
 
+import { Fragment, useEffect, useState } from 'react';
 import Link from 'next/link';
-import { PagesDetail } from './PagesList';
+import Image from 'next/image';
 import { usePathname } from 'next/navigation';
+import { PagesDetail } from './PagesList';
 import { useUtilStore } from '@/store/util';
 import NewPost from '@/app/(profile)/profile/newPost/post/NewPost';
 import { IoMdAddCircleOutline } from 'react-icons/io';
@@ -10,26 +12,51 @@ import { IoMdAddCircleOutline } from 'react-icons/io';
 const NavigateMobile = () => {
   const { username } = useUtilStore();
   const pathname = usePathname();
+  const [avatar, setAvatar] = useState<string | null>();
+
+  useEffect(() => {
+    setAvatar(localStorage.getItem('avatar'));
+  }, []);
 
   return (
     <div className="desktop:hidden flex justify-center fixed z-30 inset-x-5 bottom-1">
       <div className="bg-white flex justify-around items-center gap-x-8 p-2 rounded-xl w-full tablet:w-[65%] shadow-lg">
         {username.length > 1 &&
           PagesDetail.map((page, index) => (
-            <>
+            <Fragment key={page.link}>
               {index === 2 && (
                 <div className={`flex items-start justify-start gap-x-2 h text-grayMiddle`}>
                   <NewPost icon={<IoMdAddCircleOutline className="text-2xl" />} />
                 </div>
               )}
-              <Link href={page.link === '/profile/' ? `/profile/${username}` : page.link} key={page.link}>
-                <div
-                  className={`flex items-start justify-start gap-x-2 ${pathname === page.link + username || pathname === page.link ? 'text-black' : 'text-grayMiddle'}`}
-                >
-                  {page.icon}
-                </div>
-              </Link>
-            </>
+              {page.link === '/profile/' ? (
+                <Link href={`/profile/${username}`} key={page.link}>
+                  <div
+                    className={`flex items-start justify-start gap-x-2 ${pathname === page.link + username || pathname === page.link ? 'border border-black rounded-full' : 'text-grayMiddle'}`}
+                  >
+                    <Image
+                      src={
+                        avatar
+                          ? `https://rawrrhqbuqlxiqvuhbmc.supabase.co/storage/v1/object/public/avatars/${avatar}`
+                          : '/anonymous.png'
+                      }
+                      alt="profile avatar"
+                      width={150}
+                      height={150}
+                      className="w-[30px] h-[30px] rounded-full"
+                    />
+                  </div>
+                </Link>
+              ) : (
+                <Link href={page.link} key={page.link}>
+                  <div
+                    className={`flex items-start justify-start gap-x-2 ${pathname === page.link + username || pathname === page.link ? 'text-black' : 'text-grayMiddle'}`}
+                  >
+                    {page.icon}
+                  </div>
+                </Link>
+              )}
+            </Fragment>
           ))}
       </div>
     </div>
--- src/app/home/_component/utileSide/PagesList.tsx
+++ src/app/home/_component/utileSide/PagesList.tsx
@@ -1,6 +1,8 @@
 'use client';
 
+import { useEffect, useState, Fragment } from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
 import { usePathname } from 'next/navigation';
 import { useUtilStore } from '@/store/util';
 import { GoHomeFill } from 'react-icons/go';
@@ -18,19 +20,47 @@ export const PagesDetail: { name: string; link: string; icon: React.ReactNode }[
 const PagesList = () => {
   const { username } = useUtilStore();
   const pathname = usePathname();
+  const [avatar, setAvatar] = useState<string | null>();
+
+  useEffect(() => {
+    setAvatar(localStorage.getItem('avatar'));
+  }, []);
 
   return (
     <div className="flex flex-col items-start gap-3 w-full">
       {username.length > 1 &&
         PagesDetail.map((page) => (
-          <Link href={page.link === '/profile/' ? `/profile/${username}` : page.link} key={page.link}>
-            <div
-              className={`flex items-start justify-start gap-x-2 ${pathname === page.link ? 'text-black' : 'text-grayMiddle'}`}
-            >
-              {page.icon}
-              <h3 className="font-regular">{page.name}</h3>
-            </div>
-          </Link>
+          <Fragment key={page.link}>
+            {page.link === '/profile/' ? (
+              <Link href={`/profile/${username}`}>
+                <div
+                  className={`flex items-start justify-start gap-x-2 ${pathname === page.link + username || pathname === page.link ? 'text-black' : 'text-grayMiddle'}`}
+                >
+                  <Image
+                    src={
+                      avatar
+                        ? `https://rawrrhqbuqlxiqvuhbmc.supabase.co/storage/v1/object/public/avatars/${avatar}`
+                        : '/anonymous.png'
+                    }
+                    alt="profile avatar"
+                    width={150}
+                    height={150}
+                    className="w-[25px] h-[25px] rounded-full"
+                  />
+                  <h3 className="font-regular">{page.name}</h3>
+                </div>
+              </Link>
+            ) : (
+              <Link href={page.link}>
+                <div
+                  className={`flex items-start justify-start gap-x-2 ${pathname === page.link + username || pathname === page.link ? 'text-black' : 'text-grayMiddle'}`}
+                >
+                  {page.icon}
+                  <h3 className="font-regular">{page.name}</h3>
+                </div>
+              </Link>
+            )}
+          </Fragment>
         ))}
     </div>
   );

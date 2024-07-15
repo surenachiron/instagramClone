f45ad78f777910391c5709c2448410825883f092
Mohammad Karimi <mohammadkarimi.swctw@gmail.com>
2024-07-15 13:12:01 +0330

    feat: add get following hook fetch

---
+++ src/supabase/getFollowing.ts
@@ -0,0 +1,12 @@
+import { cache } from 'react';
+import { supabaseServer } from './utils/server';
+
+export const getFollowings = cache(async (username: string) => {
+  const supabase = supabaseServer();
+  const { data, error } = await supabase
+    .from('follows')
+    .select(`followed:profiles!follows_followed_id_fkey (user_id, user_name, avatar_url, full_name)`)
+    .eq('follower_id', username)
+    .order('created_at', { ascending: false });
+  return { data, error };
+});

8e27855282c6fa64f61a49a0386370bc1bef23d0
Mohammad Karimi <mohammadkarimi.swctw@gmail.com>
2024-07-15 13:12:24 +0330

    feat: add get profiles hook fetch

---
+++ src/supabase/getProfiles.ts
@@ -0,0 +1,11 @@
+import { cache } from 'react';
+import { supabaseServer } from './utils/server';
+
+export const getSingleProfile = cache(async (username: string) => {
+  const supabase = supabaseServer();
+
+  const { data, error } = await supabase.from('profiles').select('*').eq('user_name', username!).single();
+
+  if (error) return error;
+  return data;
+});

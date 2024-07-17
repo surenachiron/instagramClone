d6fcfd3b1d503ab00342307bb274193e97534254
Mohammad Karimi <mohammadkarimi.swctw@gmail.com>
2024-07-17 16:14:19 +0330

    chore: add more reusability

--- src/components/CopyLink.tsx
+++ src/components/CopyLink.tsx
@@ -4,11 +4,11 @@ import { useState } from 'react';
 import { usePathname } from 'next/navigation';
 import Button from './Button';
 
-const CopyLink = ({ customPath }: { customPath?: string }) => {
+const CopyLink = ({ text = 'Copy', customPath, classes }: { text?: string; customPath?: string; classes?: string }) => {
   const pathname = usePathname();
   const [copied, setCopied] = useState(false);
 
-  const copyLink = async () => {
+  const copyLinkFun = async () => {
     try {
       const link = 'https://instagram-clone-alpha-one.vercel.app' + (customPath ? customPath : pathname);
       await navigator.clipboard.writeText(link);
@@ -20,8 +20,8 @@ const CopyLink = ({ customPath }: { customPath?: string }) => {
   };
 
   return (
-    <Button onClick={copyLink} classes="py-1 px-2 rounded-lg block" direction="row" justify="start">
-      {copied ? <span className="text-green-500">Link copied.</span> : 'Copy'}
+    <Button onClick={copyLinkFun} classes={`py-1 px-2 rounded-lg block ${classes}`} direction="row" justify="start">
+      {copied ? <span className="text-green-500">Link copied.</span> : text}
     </Button>
   );
 };

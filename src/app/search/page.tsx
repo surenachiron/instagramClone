3c892b32b52846154ea1207c8d618120238bcf8c
Mohammad Karimi <mohammadkarimi.swctw@gmail.com>
2024-07-17 16:16:45 +0330

    chore: fix some issues style and useless code

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -3,7 +3,7 @@ import SearchBox from '@/components/search/SearchBox';
 const SearchPage = () => {
   return (
     <div className="w-full h-full py-2 flex justify-center">
-      <div className="w-3/4">
+      <div className="w-[90%]">
         <SearchBox bg="Back" paddingH={3} preIcon="hidden" />
       </div>
     </div>

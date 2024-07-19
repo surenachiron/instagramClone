87bd32520903fcc74ce9b76479835be3e7271da7
Mohammad Karimi <mohammadkarimi.swctw@gmail.com>
2024-07-19 10:45:09 +0330

    feat: add loading for comment in  modal

--- src/app/@modal/(.)posts/comments/[comments]/loading.tsx
+++ src/app/@modal/(.)posts/comments/[comments]/loading.tsx
@@ -1,7 +1,32 @@
-import CommentsLoading from '@/app/posts/comments/[comments]/loading';
+import { ModalRoute } from '../../[post]/ModalRoute';
+import Skeleton from '@/components/Skeleton';
 
 const LoadingCommentsIntersectionPage = () => {
-  return <CommentsLoading />;
+  return (
+    <ModalRoute>
+      <div className="flex flex-col justify-between w-full bg-white text-black overflow-auto desktop:w-1/2 border-0 h-[100vh] desktop:h-[80vh] px-3 pb-12 tablet:pb-0">
+        <div className="flex items-center gap-x-2 mb-4 mt-3">
+          <Skeleton classes="w-[25px] h-[25px]" radius="full" />
+          <div className="flex flex-col gap-y-2 w-full">
+            <Skeleton classes="py-0.5 w-[60px]" radius="full" />
+            <Skeleton classes="py-0.5 w-[120px]" radius="full" />
+          </div>
+        </div>
+        <hr />
+        <div className="h-full mt-3">
+          {Array.from({ length: 9 }, (_, colIndex) => (
+            <div className="flex items-center gap-x-2 mb-4" key={colIndex}>
+              <Skeleton classes="w-[25px] h-[25px]" radius="full" />
+              <div className="flex flex-col gap-y-2 w-full">
+                <Skeleton classes="py-0.5 w-[60px]" radius="full" />
+                <Skeleton classes="py-0.5 w-[120px]" radius="full" />
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </ModalRoute>
+  );
 };
 
 export default LoadingCommentsIntersectionPage;

162b9513a714e45070d19d876e2bc36cb495dea0
Mohammad Karimi <mohammadkarimi.swctw@gmail.com>
2024-07-22 23:42:25 +0330

    style: increase z-index

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -17,7 +17,7 @@ const Spinner = () => {
   return (
     <>
       {loading.main && (
-        <div className="absolute w-full h-full flex items-center justify-center left-0 bottom-0 bg-[#0000005c] z-50">
+        <div className="absolute w-full h-full flex items-center justify-center left-0 bottom-0 bg-[#0000005c] z-[999999999]">
           <InstagramSVG />
         </div>
       )}
